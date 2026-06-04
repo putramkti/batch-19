6eb0f03b754166404a1d286552d30412afdf09ff
Putra Jadi Mukti <putramukti26@gmail.com>
2026-06-04 11:19:35 +0700

    Add FooBar project

---
+++ FooBar/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = 15;
+
+List<string> result = new List<string>();
+for (int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        result.Add("FooBar");
+    }
+    else if (i % 3 == 0)
+    {
+        result.Add("Foo");
+    }
+    else if (i % 5 == 0)
+    {
+        result.Add("Bar");
+    }
+    else
+    {
+        result.Add(i.ToString());
+    }
+
+}
+    String output = string.Join(", ", result);
+    Console.WriteLine(output);
