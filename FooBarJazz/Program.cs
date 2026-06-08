8eb40f3390636ef428b390abefec9198060186c3
Putra Jadi Mukti <putramukti26@gmail.com>
2026-06-08 10:01:54 +0700

    add input validation for positive integers

--- FooBarJazz/Program.cs
+++ FooBarJazz/Program.cs
@@ -1,31 +1,38 @@
 ﻿Console.Write("Input number: ");
-int n = Convert.ToInt32(Console.ReadLine());
+string input = Console.ReadLine();
 
-List<string> result = new List<string>();
-for (int i = 1; i <= n; i++)
+if (int.TryParse(input, out int n) && n > 0)
 {
-    string value = "";
+    List<string> result = new List<string>();
+    for (int i = 1; i <= n; i++)
+    {
+        string value = "";
 
-    if (i % 3 == 0)
-    {
-        value += "foo";
-    }
-    if (i % 5 == 0)
-    {
-        value += "bar";
-    }
-    if (i % 7 == 0)
-    {
-        value += "jazz";
-    }
-    if (string.IsNullOrEmpty(value))
-    {
-        value = i.ToString();
+        if (i % 3 == 0)
+        {
+            value += "foo";
+        }
+        if (i % 5 == 0)
+        {
+            value += "bar";
+        }
+        if (i % 7 == 0)
+        {
+            value += "jazz";
+        }
+        if (string.IsNullOrEmpty(value))
+        {
+            value = i.ToString();
+        }
+
+        result.Add(value);
+
     }
 
-    result.Add(value);
-
+    String output = string.Join(", ", result);
+    Console.WriteLine(output);
 }
-
-String output = string.Join(", ", result);
-Console.WriteLine(output);
+else
+{
+    Console.WriteLine("Please enter a valid positive integer!.");
+}
