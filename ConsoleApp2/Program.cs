0507ed4ad6698087504d812ad8485aed9feac4c2
Putra Jadi Mukti <putramukti26@gmail.com>
2026-06-08 09:48:16 +0700

    Add ConsoleApp2 and FooBarJazz projects

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿// int a = 1;
+// Console.WriteLine(a);
+
+// Console.WriteLine(a++);
+// Console.WriteLine(a);
+
+// int b = 1;
+// Console.WriteLine(b);
+// Console.WriteLine(++b);
+// Console.WriteLine(b);
+
+
+// Console.WriteLine(5/0);
+// checked
+// {
+//     Console.WriteLine(5 / 0);
+// }
+
+// string s = null;
+// string defaultValue = "default value";
+// Console.WriteLine(s ??= defaultValue);
+
+
+// int x = 1;
+// Console.WriteLine($"value of x is {x}");
+
+// string  a = null;
+// string b = a ??= "default value";
+
+// Console.WriteLine($"value of a is {a}");
+// Console.WriteLine($"value of b is {b}");
+
+
+// string[] words = null;
+// string word = words[1];
+
+// Console.WriteLine($"value of word is {word}");
+
+
+// int a = int.MaxValue;
+// int b = 2;
+// int hasil = 0;
+// int hasil  = checked(a + b);
+
+// try
+// {
+//     // checked
+//     // {
+        
+//         hasil = a + b; 
+//     // }
+//     Console.WriteLine($"Hasil penjumlahan: {hasil}");
+// }
+// catch (System.OverflowException ex)
+// {
+//     Console.WriteLine("Error: " + ex.Message);
+// }
+
+
+// Console.WriteLine(3/0);
+// Console.WriteLine(3.0/0.0);
+
+decimal a = 1.0m;
---
+++ FooBarJazz/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+List<string> result = new List<string>();
+for (int i = 1; i <= n; i++)
+{
+    string value = "";
+
+    if (i % 3 == 0)
+    {
+        value += "foo";
+    }
+    if (i % 5 == 0)
+    {
+        value += "bar";
+    }
+    if (i % 7 == 0)
+    {
+        value += "jazz";
+    }
+    if (string.IsNullOrEmpty(value))
+    {
+        value = i.ToString();
+    }
+
+    result.Add(value);
+
+}
+
+String output = string.Join(", ", result);
+Console.WriteLine(output);
