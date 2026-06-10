50a304a99a1a14625b6056d3e37a07dfc1a20ef5
Putra Jadi Mukti <putramukti26@gmail.com>
2026-06-10 14:59:53 +0700

    Explore event handler and delegates

---
+++ AdvancedC#/Delegates/Program.cs
@@ -0,0 +1,57 @@
+﻿SomeDelegate d = SomeMethod1;
+d += SomeMethod2;
+// d = SomeMethod2;
+d();
+void SomeMethod1() { Console.WriteLine("Method 1"); }
+void SomeMethod2() { Console.WriteLine("Method 2"); }
+delegate void SomeDelegate();
+
+
+// var sc = new SampleClass();
+
+// Callback d = sc.InstanceMethod;
+// d();
+
+// // Map to the static method:
+// d = SampleClass.StaticMethod;
+// d();
+
+// delegate void Callback();
+
+// class SampleClass
+// {
+//     public void InstanceMethod()
+//     {
+//         Console.WriteLine("A message from the instance method.");
+//     }
+
+//     static public void StaticMethod()
+//     {
+//         Console.WriteLine("A message from the static method.");
+//     }
+// }
+
+
+
+// var myCar = new Car { Name = "Tesla Model S" };
+
+// Func<Vehicle> vehicleFactory = () => new Car { Name = "Porsche 911" };
+// Vehicle newVehicle = vehicleFactory();
+// Console.WriteLine($"[Func Covariance] Created: {newVehicle.Name}");
+
+// Action<Car> carInspector = InspectVehicle;
+// carInspector(myCar);
+
+// void InspectVehicle(Vehicle vehicle)
+// {
+//     Console.WriteLine($"[Action Contravariance] Inspecting: {vehicle.Name}");
+// }
+
+// public class Vehicle
+// {
+//     public string Name { get; set; } = "Generic Vehicle";
+// }
+
+// public class Car : Vehicle
+// {
+// }
---
+++ AdvancedC#/EventHandler/Program.cs
@@ -0,0 +1,52 @@
+﻿Thermometer thermometer = new Thermometer();
+
+thermometer.TemperatureChanged += (sender, e) => 
+{
+    if (e.NewTemperature > 35)
+    {
+        Console.WriteLine($"[ALARM]: Bahaya! Suhu terlalu panas: {e.NewTemperature}°C!");
+    }
+};
+thermometer.TemperatureChanged += (sender, e) =>
+{
+    if (e.NewTemperature > 100)
+    {
+        Console.WriteLine("DUARRR!");
+    }
+};
+
+// Simulasi perubahan suhu
+thermometer.ChangeTemperature(28); // Aman, tidak membunyikan alarm
+thermometer.ChangeTemperature(40); // Memicu alarm!
+thermometer.ChangeTemperature(105); 
+
+
+// 1. DATA EVENT(Wajib akhiran EventArgs)
+public class TemperatureChangedEventArgs : EventArgs
+{
+    public int NewTemperature { get; }
+    public TemperatureChangedEventArgs(int temp) => NewTemperature = temp;
+}
+
+// 2. PUBLISHER
+public class Thermometer
+{
+    // Deklarasi Event Standar .NET
+    public event EventHandler<TemperatureChangedEventArgs> TemperatureChanged;
+
+    // Pelatuk Event (Wajib awalan "On", bertipe protected virtual)
+    protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
+    {
+        TemperatureChanged?.Invoke(this, e); // Aman dari error null & multi-thread
+    }
+
+    public void ChangeTemperature(int currentTemp)
+    {
+        Console.WriteLine($"[Termometer]: Suhu bergeser ke {currentTemp}°C");
+        
+        // Picu pelatuk dan kirim datanya
+        OnTemperatureChanged(new TemperatureChangedEventArgs(currentTemp));
+    }
+}
+
+
