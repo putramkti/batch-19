3ba09989cb17910b63058f9617b0c4fa080503a1
Putra Jadi Mukti <putramukti26@gmail.com>
2026-06-22 10:09:51 +0700

    update FooBarJazz project to final logic exercise

--- FooBarJazz/Program.cs
+++ FooBarJazz/Program.cs
@@ -1,46 +1,64 @@
-﻿Console.Write("Input number: ");
+﻿using System.Data;
+
+MyClass myClass = new MyClass();
+myClass.AddRule(3, "foo");
+myClass.AddRule(4, "baz");
+myClass.AddRule(5, "bar");
+myClass.AddRule(7, "jazz");
+myClass.AddRule(9, "huzz");
+
+Console.Write("Input number: ");
 string input = Console.ReadLine();
 
 if (int.TryParse(input, out int n) && n > 0)
 {
-    List<string> result = new List<string>();
-    for (int i = 1; i <= n; i++)
-    {
-        string value = "";
-
-        if (i % 3 == 0)
-        {
-            value += "foo";
-        }
-        if (i % 4 == 0)
-        {
-            value += "baz";
-        }
-        if (i % 5 == 0)
-        {
-            value += "bar";
-        }
-        if (i % 7 == 0)
-        {
-            value += "jazz";
-        }
-        if( i % 9 == 0)
-        {
-            value += "huzz";
-        }
-        if (string.IsNullOrEmpty(value))
-        {
-            value = i.ToString();
-        }
-
-        result.Add(value);
-
-    }
-
-    String output = string.Join(", ", result);
-    Console.WriteLine(output);
+    myClass.Generator(n);
 }
 else
 {
     Console.WriteLine("Please enter a valid positive integer!.");
+}
+
+public class MyClass
+{
+    private Dictionary<int, string> _rules = new Dictionary<int, string>();
+
+    public void AddRule(int input, string output)
+    {
+        // if(_rules.TryAdd(input, output) && input > 0)
+        // {
+
+        // }
+        _rules.Add(input, output);
+    }
+
+    public void Generator(int n)
+    {
+        List<string> result = new List<string>();
+        for (int i = 1; i <= n; i++)
+        {
+            string value = "";
+
+            // var _sortedRules = _rules.OrderBy(x => x.Key);
+            foreach (var rule in _rules.OrderBy(x => x.Key))
+            {
+                if (i % rule.Key == 0)
+                {
+                    value += rule.Value;
+                }
+            }
+
+            if (string.IsNullOrEmpty(value))
+            {
+                value = i.ToString();
+            }
+
+            result.Add(value);
+
+        }
+
+        string output = string.Join(", ", result);
+        Console.WriteLine(output);
+    }
+
 }
